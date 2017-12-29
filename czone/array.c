e3963ef22f1fb6944e7f563f218dda3436abd7ca
Moguls <1987932553@qq.com>
2017-12-29 09:18:08 +0800

    Array_address

--- czone/array.c
+++ czone/array.c
@@ -1,3 +1,17 @@
+/*
+for the array int a[4]={11,12,13,14};
+*/
+/*
+The string  of b  =aaa;
+The string  of c  =aa;
+The value of   a  =6295616;
+The value of &a[0]=6295616.
+The value of size_t num =500.
+The address of a[0] is 0x601040.
+The address of a[1] is 0x601044.
+The address of a[2] is 0x601048.
+The address of a[3] is 0x60104c.
+*/
 #include<stdio.h>
 
 	int a[4]={11,12,13,14};
@@ -5,13 +19,18 @@
 	char c[2]={"aa"};
 	size_t num=500;
 	int main(){
-
+	printf("/*\n *for the array int a[4]={11,12,13,14};\n*/\n\n");
 	printf("The string  of b  =%s;\n",b);
 	printf("The string  of c  =%s;\n",c);
 
 	printf("The value of   a  =%ld;\n",a);
 	printf("The value of &a[0]=%ld.\n",&a[0]);
 	printf("The value of size_t num =%d.\n",num);
+
+	for(int i=0;i<4;i++)
+	{
+		printf("The address of a[%d] is %p.\n",i,&a[i]);
+	}
 
 return 0;
 }
